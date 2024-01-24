2c37da08617a67ed230b08073ad2607143454c23
Julia Näsström <jnass@kth.se>
2024-01-24 17:13:00 +0100

    New
    New

--- Temp_and_humidity 2/main_STRAIGHTNER.py
+++ Temp_and_humidity 2/main_STRAIGHTNER.py
@@ -1,11 +1,10 @@
 #-------------------------------IMPORT------------------------------------------
+import machine
 import time
 from machine import I2C
 import math
-import machine
 import pycom
 from SI7006A20 import SI7006A20 # Import the class in order to make object later
-from mqtt import MQTTClient # Import the class in order to make object later
 #-------------------------------------GET VALUES--------------------------------
 '''This function takes in noting as an argument, creates an SI7006A20 object
      which we can use to get sensor data on external temprature in celcius.
@@ -18,34 +17,8 @@ def get_data():
     # to what is presented on Datacakes
     print(int(temp)) # Easier on the eye to get an integer presented!
     return temp
-#----------------------------CONNECT TO DATACAKES-------------------------------
-# Note: This code is derived from https://hackmd.io/@lnu-iot/r1aui0B59#Datacake-part
 
-# These variables are specific to me, you will need to change them accordingly
-SERIAL_NUMBER = '1234Julia1234'
-MQTT_BROKER = 'mqtt.datacake.co'
-TOKEN = '48c27ecc04e0671cddb6b426e0cdb73fe065dca9'
-PORT = 1883
 
-''' A function which takes in a topic an message and then prints the message.
-    Returns nothing'''
-def sub_cb(topic, msg):
-   print(msg)
-
-# MQTT Setup
-client = MQTTClient(SERIAL_NUMBER,
-                    MQTT_BROKER,
-                    user=TOKEN,
-                    password=TOKEN,
-                    port=PORT)
-client.set_callback(sub_cb)
-client.connect()
-print('connected to MQTT broker') # If this does not get printed someting is wrong!
-
-#----------------------------------PUBLISH DATA---------------------------------
-# The MQTT topic that we publish data to
-my_topic_temp = 'dtck-pub/temp-4/992aa037-ba50-4bbc-b67f-c73fc83bb789/TEMPRATURE'
-my_topic_cooldown = 'dtck-pub/temp-4/992aa037-ba50-4bbc-b67f-c73fc83bb789/COOLDOWN'
 
 pycom.heartbeat(False)
 
@@ -54,9 +27,7 @@ Heating = True
 while Heating: # Whilst we heat up the hot tool, get temprature and publish it
     pycom.rgbled(0xFF0000)  # Blink red during this phase
     temp = get_data()
-    client.publish(my_topic_temp, msg=str(int(temp)))
-
-    print("Send data to MQTT broker, sleeping for 10 sec...") # Confirmation that we are resing
+    print(str(int(temp)))
     time.sleep(10)
 
     # Note: This threshold might be different for different people, play around with it!
@@ -72,14 +43,12 @@ i = 0 # Our cooldown time will be calculated using a simple counter
 while Cooldown:
     pycom.rgbled(0x0000FF) # Blink Blue during this phase
     temp = get_data()
-    client.publish(my_topic_temp, msg=str(int(temp)))
-    print("Send data to MQTT broker, sleeping for 10 sec...")
+    print(str(int(temp)))
     time.sleep(10)
     i += 10 # Add 10 sec to counter (Since we waited 10 since last time)
 
     # Note: This threshold might be different for different people, play around with it!
     if temp < 36: # Change if needed
         print('cooldown time done: ', i, 'Secs')
-        client.publish(my_topic_cooldown, msg=str(int(i))) # Publish this!
         Cooldown = False
 #------------------------------------------------------------------------------
